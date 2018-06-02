6af4258fffa252327d03d0a7c80cbd1c86631617
yumikomatsumoto <matsumoto.0124.dm@gmail.com>
2018-06-02 22:57:27 +0900

    no message

---
+++ Assets/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+
+
+	// Use this for initialization
+	void Start () {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            //ボールが画面外に出た場合
+            if(this.transform.position.z < this.visiblePosZ) {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+		
+	} 
+}
---
+++ Assets/ScoreText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour{
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコア用の変数
+    private int score = 0;
+
+    // Use this for initialization
+    void Start(){
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other){
+
+        //当たったオブジェクトに応じてスコアを足しわける
+        if (other.gameObject.tag == "SmallStarTag") {
+            Debug.Log(other.gameObject.name);
+            score += 5;
+        } else if (other.gameObject.tag == "SmallCloudTag"){
+            Debug.Log(other.gameObject.name);
+            score += 10;
+        } else if (other.gameObject.tag == "LargeCloudTag"){
+            Debug.Log(other.gameObject.name);
+            score += 20;
+        } else if (other.gameObject.tag == "LargeStarTag"){
+            Debug.Log(other.gameObject.name);
+            score += 30;
+        }
+
+        //スコアをテキストに反映させる
+        this.scoreText.GetComponent<Text>().text = "Score:" + score;
+    }
+}
